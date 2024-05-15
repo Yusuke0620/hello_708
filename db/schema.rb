1e4520a85c4c0a143acca3f2fbb4383d0b3f6a59
Yusuke0620 <boston19851229@gmail.com>
2024-05-15 18:31:33 +0900

    create Question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_15_072600) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_15_091919) do
+  create_table "questions", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
