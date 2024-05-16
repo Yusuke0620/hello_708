a72b76934fe2d8d2623aab0e74b6ada7525bc296
Yusuke0620 <boston19851229@gmail.com>
2024-05-16 16:26:16 +0900

    バリデーション追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,35 +1,46 @@
 class QuestionsController < ApplicationController
   # 質問一覧表示
   def index
+    @questions = Question.all
+    p @questions
   end
 
   # 質問詳細表示
   def show
-
+    # p params
+    @question = Question.find(params[:id])
+    # p @question
   end
 
   # 質問の作成
   def new
-
+    @question = Question.new
   end
 
   # 質問の登録
   def create
-
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to @question # showへリダイレクト
+    else
+      render "new", status: :unprocessable_entity
+    end
   end
 
   # 質問の編集
   def edit
-
   end
 
   # 質問の更新
   def update
-
   end
 
   # 質問の削除
   def destroy
+  end
 
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'users' => 'users#index'
-
+  resources :questions
 end
